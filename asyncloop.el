22d299cd0ba0c8d407acba6cb22ed8f7be4161a6
Martin Edström <meedstrom91@gmail.com>
2024-03-23 20:01:59 +0100

    Polish

--- asyncloop.el
+++ asyncloop.el
@@ -1,17 +1,17 @@
 ;;; asyncloop.el --- Non-blocking series of functions -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2022-2024 Martin Edström
-
+;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
@@ -19,7 +19,7 @@
 
 ;; Author: Martin Edström <meedstrom91@gmail.com>
 ;; Created: 2022-10-30
-;; Version: 1.1
+;; Version: 1.2-pre
 ;; Keywords: tools
 ;; Homepage: https://github.com/meedstrom/asyncloop
 ;; Package-Requires: ((emacs "28"))
@@ -75,7 +75,7 @@ passing it to `display-warning' or `error' or the like."
         (text (apply #'format args))
         (was-at-eob nil))
     (when (and buf (buffer-live-p buf))
-      (save-excursion
+      (save-excursion ;; needed to prevent scrolling
         (with-current-buffer buf
           (setq was-at-eob (eobp))
           (goto-char (point-max))
@@ -91,7 +91,7 @@ passing it to `display-warning' or `error' or the like."
         ;; because they don't redisplay on every insertion, instead batching
         ;; updates in chunks of 10-20 lines.  This reduces that choppiness.
         (redisplay)))
-    ;; Allow the convenient sexp (warn "%s" (asyncloop-log loop "msg"))
+    ;; Allow user to write e.g. (warn "%s" (asyncloop-log loop "msg"))
     text))
 
 (defun asyncloop-reset-all ()
@@ -114,7 +114,7 @@ Meant to be bound in asyncloop log buffers."
        when (eq (current-buffer) (asyncloop-log-buffer loop))
        do (progn (asyncloop-cancel loop 'quietly)
                  (asyncloop-log loop
-                   "Reset due to quit in buffer %s" (buffer-name))))
+                   "Loop reset due to quit in buffer %s" (buffer-name))))
     (keyboard-quit)))
 
 (defvar asyncloop-log-mode-map
@@ -213,7 +213,7 @@ This is the intended way to call `asyncloop-eat', because
 (defun asyncloop-chomp (loop)
   "Run functions that remain in LOOP, one at a time."
   (asyncloop-with-slots (remainder starttime paused immediate-break-on-user-activity) loop
-    ;; In case the last function pushed t on REMAINDER and got interrupted
+    ;; In case the last function pushed t on REMAINDER and got interrupted (rare)
     (when (not (functionp (car remainder)))
       (pop remainder))
     (asyncloop-clock-funcall loop (car remainder))
